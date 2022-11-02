c176018bf0f2249e2e2ddc7edce41649f0535afb
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-02 22:16:36 +0530

    new route created

--- Router/Selfcheck.js
+++ Router/Selfcheck.js
@@ -6,7 +6,7 @@ router.get("/", async (req, res) => {
   try {
     const Check = await Checkin.find()
     if (!Check) {
-      throw new Error("no new a")
+      throw new Error("no new data")
     }
 
     res.status(200).json(Check)
@@ -20,16 +20,28 @@ router.post("/", async (req, res) => {
     const Check = await new Checkin({
       Passengername: req.body.Passengername,
       Phonenumber: req.body.Phonenumber,
-      Email: req.body.Phonenumber,
+      Email: req.body.Email,
       Flightnumber: req.body.Flightnumber,
       Destination: req.body.Destination,
-      Counternumber: req.body.Counternumber,
+      Counternumber: Math.floor(Math.random() * 5) + 1,
     }).save()
 
     if (!Check) {
       throw new Error("no transactions saved")
     }
 
+    res.status(200).json(Check)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
+router.get("/:id", async (req, res) => {
+  try {
+    const Check = await Checkin.find({ _id: req.params.id })
+    if (!Check) {
+      throw new Error("no new data")
+    }
     res.status(200).json(Check)
   } catch (error) {
     res.status(400).json({ message: error.message })
--- models/Checkin.js
+++ models/Checkin.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-let counterno = Math.floor(Math.random() * 5)
+let counterno = Math.floor(Math.random() * 5) + 1
+let date = new Date(Date.now()).getMonth() + 1
 const selfCheckin = new mongoose.Schema({
   Passengername: {
     type: String,
@@ -24,6 +25,19 @@ const selfCheckin = new mongoose.Schema({
   Counternumber: {
     type: Number,
     default: counterno,
+  },
+  Date: {
+    type: String,
+    default:
+      new Date(Date.now()).getDate() +
+      "/" +
+      date +
+      "/" +
+      new Date(Date.now()).getFullYear(),
+  },
+  Time: {
+    type: Number,
+    default: new Date(Date.now()).getTime(),
   },
 })
 module.exports.Checkin = mongoose.model("Checkin", selfCheckin)
